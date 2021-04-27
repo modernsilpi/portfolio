faa6448125bdada7364f0cd5fdeb35571b1d2f13
modernsilpi <modernsilpi@gmail.com>
2021-04-27 23:45:02 +0530

    new stylings

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,19 +1,51 @@
 import React from 'react'
-import hemanth from './navbar/hemanth.JPG';
+import hemanth3 from './navbar/hemanth3.jpg';
 import "./portfolio.css";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
+import router from "./navbar/router.jpg";
+import website from "./navbar/website.jpg";
+import hacker from "./navbar/hacker.jpg";
 
 function Portfolio() {
     return (
+        <>
         <div className="portfolio" >
             <h1>Hemanth</h1>
             <p>Developer, Pentester.</p>
-            <img src={ hemanth } alt="hemanth" className="ProfilePic" />
+            <img src={ hemanth3 } alt="hemanth" className="ProfilePic" />
             <div>
                 <Button variant="success">Recent Projects </Button>
                 <Button style={{marginLeft: "20px"}} variant="success">Contact me</Button>
             </div>
         </div>
+        <section className="cardHolder">
+            <Row xs={12} md={12} lg={12} className="row">
+                <Col className="col">
+                <div className="Displaycard">
+                  <img src={ hacker } alt="Cyber Security" />
+                  <h2>Cyber Security</h2>
+                </div>
+                </Col>
+                <Col className="col">
+                <div className="Displaycard">
+                  <img src={ router } alt="Networking" />
+                  <h2>Networking</h2>
+                </div>
+                </Col>
+                <Col className="col">
+                <div className="Displaycard">
+                  <img src={ website } alt="Web Designing" />
+                  <h2>Web Designing</h2>
+                </div>
+                </Col>
+            </Row>
+        </section>
+        <footer>
+            <p>Instagram</p>
+            <p>LinkedIn</p>
+            <p>Twitter</p>
+        </footer>
+        </>
     )
 }
 
