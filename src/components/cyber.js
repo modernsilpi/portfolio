19503a1070145135df076ef90afec119a16a8da5
modernsilpi <modernsilpi@gmail.com>
2021-04-28 23:48:23 +0530

    cyber content

--- src/components/cyber.js
+++ src/components/cyber.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import data from './cyberData';
+import "./cyber.css";
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus className="plus" /> : <AiOutlinePlus className="plus" />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
 
 function Cyber() {
+    const [questions, setQuestions] = useState(data);
     return (
-        <div>
+        <div className="cyberPage">
             <h2>Cyber security</h2>
+            <section className='info'>
+          {questions.map((question) => {
+            return (
+              <Question key={question.id} {...question}></Question>
+            );
+          })}
+        </section>
         </div>
     )
 }
---
+++ src/components/cyberData.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Skills Secured',
+      info:
+        'Kali Linux, Linux cli, Networking, Burp suit.',
+    },
+    {
+      id: 2,
+      title: 'Tools Experienced',
+      info:
+        'BurpSuit, Metasploit, Wireshark, Nmap, Recon-ng, Tor, OSINT Framework, Google Dorks.',
+    },
+    {
+      id: 3,
+      title: 'Interested Domain',
+      info:
+        'Web Application penetration testing and Mobile application penetration testing',
+    },
+    {
+      id: 4,
+      title: 'Goal for next 5 years',
+      info:
+        'To achieve OSCP license.',
+    }
+  ]
+  export default questions
+  
