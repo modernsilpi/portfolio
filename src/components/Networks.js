5add2b2a402de081695f19462f1f26fd69d2e517
modernsilpi <modernsilpi@gmail.com>
2021-04-30 16:48:06 +0530

    network

--- src/components/Networks.js
+++ src/components/Networks.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import data from './networkdata';
+import "./cyber.css";
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus className="plus" /> : <AiOutlinePlus className="plus" />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
 
 function Networks() {
+    const [questions, setQuestions] = useState(data);
     return (
-        <div>
-            Networks
+        <div className="cyberPage">
+            <h2>Networks</h2>
+            <section className='info'>
+          {questions.map((question) => {
+            return (
+              <Question key={question.id} {...question}></Question>
+            );
+          })}
+        </section>
         </div>
     )
 }
---
+++ src/components/networkdata.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Skills Secured',
+      info:
+        'Gone through Comptia network+ course.',
+    },
+    {
+      id: 2,
+      title: 'Tools Experienced',
+      info:
+        'Wireshark, Nmap, netstat, Linux.',
+    },
+    {
+      id: 3,
+      title: 'Interested Domain',
+      info:
+        'Network Administration.',
+    },
+    {
+      id: 4,
+      title: 'Goal for next 2 years',
+      info:
+        'Get positioned as a network administrator.',
+    }
+  ]
+  export default questions
+  
